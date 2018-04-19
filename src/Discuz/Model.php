831d1f6c794f6b26f04c22dfe84f11e3af8f6a72
panyian <panyian82@qq.com>
2018-04-20 07:27:21 +0800

    gitignore

---
+++ src/Discuz/Model.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pan
+ * Date: 2018/4/18
+ * Time: 下午11:56
+ */
+
+namespace Discuz;
+use Illuminate\Database\Eloquent\Model as EloModel;
+
+class Model extends EloModel
+{
+    public $timestamps = false;
+}
